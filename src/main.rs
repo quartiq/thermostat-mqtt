75fc7e27cf8346c11ca35c5b9cc975b064cb98ce
SingularitySurfer <norman_krackow@gmx.de>
2021-10-05 12:27:29 +0200

    fix wrong iir offset

--- src/main.rs
+++ src/main.rs
@@ -223,7 +223,9 @@ const APP: () = {
 
         for (i, iir) in c.resources.iirs.iter_mut().enumerate() {
             info!("dacdata:\t {:?}", c.resources.settings.pidsettings[i]);
-            iir[0].set_x_offset(c.resources.settings.pidsettings[i].target);
+            iir[0].set_x_offset(temp_to_iiroffset(
+                c.resources.settings.pidsettings[i].target,
+            ));
             iir[0].ba = pid_to_iir(c.resources.settings.pidsettings[i].pid);
             iir[0].y_min = c.resources.settings.pidsettings[i].min;
             iir[0].y_max = c.resources.settings.pidsettings[i].max;
